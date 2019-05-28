56c17a1152315706e16f8595afdab4bf3c959da6
Erica Norris <norrise120@gmail.com>
2019-05-28 15:53:49 -0700

    Logic to allow play on click

--- noise.js
+++ noise.js
@@ -1,3 +1,20 @@
+const notesHash = {
+  'a': 'aAudio',
+  'b': 'bAudio',
+  'c': 'cAudio',
+  'd': 'dAudio',
+  'e': 'eAudio',
+  'f': 'fAudio',
+  'g': 'gAudio',
+};
+
 $(document).ready( function() {
-  // your code here
+  $('button').click( function() {
+    let noteLetter = this.getAttribute("class");
+    noteLetter = noteLetter.split(' ')[1];
+    
+    const audio = document.getElementById(notesHash[noteLetter]);
+    audio.play();
+    audio.currentTime = 0
+  })
 });
